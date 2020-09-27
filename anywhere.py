5be1fe354ff0af1ac1ece6a82f134935cab7f7ae
HP <krishnavamsiyendamuri@gmail.com>
2020-09-27 22:17:14 +0530

    just modified!

--- anywhere.py
+++ anywhere.py
@@ -1,2 +1,13 @@
 print("hi I am Y.Sai Sidhardh")
 print("i have edited this using git hub from online")
+print("this print line i have edited using pycharm")
+"""i am giving a code using python IDE (Py Charm) for finding prime numbers"""
+num = int(input("enter the number you want to check: "))
+half = num//2
+for i in range(2,half):
+    if num % i == 0:
+        print("{} is not a prime number".format(num))
+        break
+else:
+    print("{} is a prime number".format(num))
+print("thank you!")

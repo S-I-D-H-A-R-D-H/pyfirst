7ca5cab041cc5eead87cb7715c4e5a790180b78d
S-I-D-H-A-R-D-H <71966854+S-I-D-H-A-R-D-H@users.noreply.github.com>
2020-10-02 18:00:18 +0530

    Update anywhere.py

--- anywhere.py
+++ anywhere.py
@@ -1,6 +1,4 @@
 print("hi I am Y.Sai Sidhardh")
-print("i have edited this using git hub from online")
-print("this print line i have edited using pycharm")
 """i am giving a code using python IDE (Py Charm) for finding prime numbers"""
 num = int(input("enter the number you want to check: "))
 half = num//2
@@ -10,5 +8,3 @@ for i in range(2,half):
         break
 else:
     print("{} is a prime number".format(num))
-print("hope you got the right output")
-print("thank you!")
